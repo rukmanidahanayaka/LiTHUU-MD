368aebdb07942d20d0b9de4270c70f12f92a628b
Lithuwaofficiall <gaminglithu8@gmail.com>
2024-11-30 19:10:19 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tPNQjmX/20241128-184557.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Alive Now👋"
+};
